605b1f6d7d4f2090b2392608667e1c010ab037f3
Cooper <cooper.davies@vizworx.com>
2023-05-16 20:10:40 -0600

    including test file to ensure that libs are properly installed

--- src/main.cpp
+++ src/main.cpp
@@ -8,28 +8,28 @@
 
 static Stack<AbstractState *> stateStack = Stack<AbstractState *>();
 int frame = 0;
-void setup()
-{
-	Serial.begin(115200);
-	DebugState *toAdd = new DebugState();
-	toAdd->i = 0;
-	stateStack.push(toAdd);
-}
+// void setup()
+// {
+// 	Serial.begin(115200);
+// 	DebugState *toAdd = new DebugState();
+// 	toAdd->i = 0;
+// 	stateStack.push(toAdd);
+// }
 
-void loop()
-{
-	if (frame == 10)
-	{
-		stateStack.push(new AnimationState());
-	}
-	if (frame == 20)
-	{
-		stateStack.pop();
-	}
-	if (frame == 30)
-	{
-		stateStack.push(new AnimationState());
-	}
-	stateStack.peek()->Cycle();
-	frame++;
-}
+// void loop()
+// {
+// 	if (frame == 10)
+// 	{
+// 		stateStack.push(new AnimationState());
+// 	}
+// 	if (frame == 20)
+// 	{
+// 		stateStack.pop();
+// 	}
+// 	if (frame == 30)
+// 	{
+// 		stateStack.push(new AnimationState());
+// 	}
+// 	stateStack.peek()->Cycle();
+// 	frame++;
+// }
